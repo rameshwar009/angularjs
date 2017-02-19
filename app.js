c22331f97771cb4fbda6f136d18bf237870d8846
rameshwar7 <rameshwar@seynse.com>
2017-02-20 00:09:48 +0530

    Injector minification line added

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 (function (){
   angular.module('LunchCheck', [])
-  .controller('LunchCheckController', function ($scope){
+  .controller('LunchCheckController', LunchCheckController)
+
+  LunchCheckController.$inject = ['$scope']
+  function LunchCheckController ($scope){
     $scope.lunchItems = '';
     $scope.message = '';
 
     $scope.checkIfTooMuch = function (){
       var nonEmptyLunchItems = getNumberOfLunchItems($scope.lunchItems)
       if (nonEmptyLunchItems == 0){
-        $scope.message = 'Empty';
+        $scope.message = 'Please Enter Data First';
       }
       else if (nonEmptyLunchItems > 3){
         $scope.message = 'Too much';
@@ -16,9 +19,11 @@
         $scope.message = 'Enjoy!!'
       }
     };
-  });
+  };
 
   // Getting Number of Non Empty Lunch items
+  // , , is considered as an item.
+  // ,, is not considered any item.
   function getNumberOfLunchItems (lunchitems){
     var lunchItemsArray = lunchitems.split(',')
     var totalLunchItems = lunchItemsArray.length
